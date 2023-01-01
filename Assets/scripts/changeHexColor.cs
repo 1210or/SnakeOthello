d4db20fdd52505f956819ee619e6b0f323a6d129
1210or <r.osaki1210@gmail.com>
2023-01-01 12:34:56 +0900

    周囲のステージを取得する関数を作成

--- Assets/scripts/changeHexColor.cs
+++ Assets/scripts/changeHexColor.cs
@@ -18,9 +18,22 @@ public class changeHexColor : MonoBehaviour
 
     void OnMouseDown() //クリックされると起動
   {
-      print("click " + this.name); //オブジェクトの名前をプリント
+      //print("click " + this.name); //オブジェクトの名前をプリント
 
       //ランダムにマテリアルカラーを変える
       this.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+      //print();
+      //周囲の色を変える(ボム)
+      for(int i = 0; i < StageHexagon.arroundHexagons(this.gameObject).Length; i++ ){
+
+        //arroundHexagons関数を使ってthis.gameObjectの隣接するヘキサゴンを取得
+        StageHexagon.arroundHexagons(this.gameObject)[i].GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+      }
+      
+ 
   }
+  
+
+
 }
--- Assets/scripts/stageMaker.cs
+++ Assets/scripts/stageMaker.cs
@@ -42,3 +42,24 @@ public class stageMaker : MonoBehaviour
 
     }
 }
+public class StageHexagon{
+  //引数に入れたゲームオブジェクト(ステージ)の接するステージを配列にして返す
+  public static GameObject[] arroundHexagons (GameObject hexagon)
+  {
+          //位置座標からインデックスに変換
+          int x = (int)(hexagon.transform.position.x - (0.5f * hexagon.transform.position.z));
+          int z = (int)(hexagon.transform.position.z);
+
+          //配列に周囲のステージを格納
+          GameObject[] arroundHexagons　= new GameObject[6];
+          try{arroundHexagons[0] = stageMaker.stageObject[x,z-1];}catch (System.Exception){arroundHexagons[0] = hexagon;}
+          try{arroundHexagons[1] = stageMaker.stageObject[x+1,z-1];}catch (System.Exception){arroundHexagons[1] = hexagon;}
+          try{arroundHexagons[2] = stageMaker.stageObject[x-1,z];}catch (System.Exception){arroundHexagons[2] = hexagon;}
+          try{arroundHexagons[3] = stageMaker.stageObject[x+1,z];}catch (System.Exception){arroundHexagons[3] = hexagon;}
+          try{arroundHexagons[4] = stageMaker.stageObject[x-1,z+1];}catch (System.Exception){arroundHexagons[4] = hexagon;}
+          try{arroundHexagons[5] = stageMaker.stageObject[x,z+1];}catch (System.Exception){arroundHexagons[5] = hexagon;}
+          //↑冗長、端で何もない場合は中心のhexagonを入れてるけれど、本当は配列自体を消したい。
+
+          return arroundHexagons;
+  }
+}
